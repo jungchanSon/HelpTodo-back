7d186b33a88f2b97a02b2e69f9bcc47d77eb02ce
ABCXZY <rnrmfjc@gmail.com>
2022-11-25 22:29:42 +0900

    feat: TodoList Repo   - todolist repo 저장, 조회 구현

--- src/main/java/HelpTodo/helptodoBackend/repository/TodoListRepository.java
+++ src/main/java/HelpTodo/helptodoBackend/repository/TodoListRepository.java
@@ -1,5 +1,28 @@
 package HelpTodo.helptodoBackend.repository;
 
+import HelpTodo.helptodoBackend.domain.Todolist;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
 public class TodoListRepository {
 
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Todolist todolist){
+        em.persist(todolist);
+    }
+
+    public List<Todolist> findAllByTeamName(String teamName){
+        List<Todolist> todoLists = em.createQuery("select tl from Todolist tl where tl.team.name = :teamName", Todolist.class)
+            .setParameter("teamName", teamName)
+            .getResultList();
+
+        return todoLists;
+    }
 }
