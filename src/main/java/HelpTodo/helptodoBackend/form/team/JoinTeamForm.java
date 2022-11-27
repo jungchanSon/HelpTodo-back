cfaceea9b6c780ead048ec45730447923f6fa7e7
ABCXZY <rnrmfjc@gmail.com>
2022-11-27 20:53:02 +0900

    feat :팀 참가 컨틀롤러

---
+++ src/main/java/HelpTodo/helptodoBackend/form/team/JoinTeamForm.java
@@ -0,0 +1,11 @@
+package HelpTodo.helptodoBackend.Form.team;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class JoinTeamForm {
+    String userId;
+    String teamName;
+}
