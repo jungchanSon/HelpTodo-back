2ea1c9f06a0bc9eff983f041652d3edeb3e12d69
ABCXZY <rnrmfjc@gmail.com>
2023-04-03 13:11:44 +0900

    build: Redis 설치

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# helpTodo-back
+
+# helpTodo-back 
+HelpTodo 웹 서비스의 백엔드 서버입니다.
+
+# 2023.04.03.
+Redis 추가
+
+- 같은 팀 내 팀원이 데이터를 수정할 경우, 1회성으로 업데이트 신호를 보내주기 위해 Redis를 이용해 Pub/Sub 구현할 예정.
+- AWS SNS와 AWS Lamda를 이용한 노팅도 생각해보기
--- src/main/java/HelpTodo/helptodoBackend/form/team/JoinTeamForm.java
+++ src/main/java/HelpTodo/helptodoBackend/form/team/JoinTeamForm.java
@@ -2,8 +2,10 @@ package HelpTodo.helptodoBackend.Form.team;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class JoinTeamForm {
     String teamName;
     String teamPassword;
