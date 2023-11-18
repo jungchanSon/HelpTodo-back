5dc67fd534591fb6fe33d8416641c00d93a3c68c
jungchanSon <rnrmfjc@gmail.com>
2023-11-18 20:58:24 +0900

    feature : add new params.

--- src/main/java/HelpTodo/helptodoBackend/form/TodoListForm/AddTDDForm.java
+++ src/main/java/HelpTodo/helptodoBackend/form/TodoListForm/AddTDDForm.java
@@ -3,12 +3,17 @@ package HelpTodo.helptodoBackend.form.TodoListForm;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Getter @Setter
 public class AddTDDForm {
 
     String teamName;
     String Content;
     Long TodoListId;
+    String startDate;
+    String endDate;
+    String manager;
 
     int importance;
 }
